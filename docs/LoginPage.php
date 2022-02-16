48ab30ce3c326da479f7976076b34c863904f78a
AntonS <anton.satalin@student.hamk.fi>
2022-02-16 13:02:32 +0200

    Create LoginPage.php

---
+++ docs/LoginPage.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+?>
+<html>
+<head>
+<title>Log in into your Il Marino account </title>
+<h1>Log into your Il Marino account</h1>    
+</head>
+<body>
+<form>
+    <label for="username"></label> Username: <br>
+    <input type="text" id="username" name="username"><br>
+    <label for="encpass"></label>Password:<br>
+    <input type="text" id="encpass" name="encpass"><br>
+    <input type="submit" value="submit"><br>
+</form>    
+</body>
+
+</html>
