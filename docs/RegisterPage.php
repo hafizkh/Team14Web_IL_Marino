4b9260382ea8cafea343367046a4a0804f683f35
AntonS <anton.satalin@student.hamk.fi>
2022-02-18 10:56:08 +0200

    Create RegisterPage.php

---
+++ docs/RegisterPage.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Create a new Il Marino account </title>
+<h1>Create a new  Il Marino account</h1>    
+</head>
+<body>
+<form>
+    <label for="username"></label> Username: <br>
+    <input type="text" id="username" name="username"><br>
+    <label for="pass"></label>Password:<br>
+    <input type="text" id="pass" name="pass"><br>
+    <label for="email"></label> Email:<br>
+    <input type="text" id="email" name="email"><br>
+    <label for="phone"></label>PhoneNumber:<br>
+    <input type="text" id="phone" name="phone"><br>
+    <input type="Register" value="Register"><br>
+
+</form>    
+</body>
+
+</html>
+
+
+<?php
+
+
+?>
