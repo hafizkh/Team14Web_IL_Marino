3e6df269082b183bf95840e7fa2530e80026bfae
AntonS <anton.satalin@student.hamk.fi>
2022-02-21 13:24:58 +0200

    Update newspage.php

--- docs/newspage.php
+++ docs/newspage.php
@@ -1,7 +1,12 @@
 <?php include 'header.php'?>
 <?php
 /* loop creating news pages from database,format them identically,limit posts on page. */
-
+$page_base="newspage.php";
+$user_id=$_POST['user_id'];
+if(strlen($user_id) > 0 and !is_numeric($user_id))
+{
+echo"Incorect Value Type";
+}
 
 
 
