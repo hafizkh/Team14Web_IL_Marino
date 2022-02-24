a7d2f8126687b7474bb64659ce794af82d260756
AntonS <anton.satalin@student.hamk.fi>
2022-02-24 15:32:31 +0200

    Styling and workability

--- docs/newspage.php
+++ docs/newspage.php
@@ -5,7 +5,7 @@
 <body>
  <?php
 
-$limitByPage=10;
+$limitByPage=5;
 $query='select * from Post' ;
 $result=mysqli_query($conn,$query);
 $totalresults=mysqli_num_rows($result);
@@ -21,25 +21,39 @@ $query="Select * FROM Post Limit" .$firstpageresult .','. $limitByPage ;
 
 while($row = mysqli_fetch_array($result))
 {
-echo '<div class="header">'. $row['Header'].'</div>'.
+echo '<div class="poster">'.
 '<div class="postid"> Post id : '.$row['Post_id'].'</div>'.
-' <div class="Emp_id"> Author id :' . $row['Employee_id'] .'</div>'.
-'<div class="date">'."<br>Date :". $row['date_ps'].'</div>'.
-"<br>".'<div class="text">'. $row['Text_ps'].'</div>'."<br>";
-?>;
+'<div class="Emp_id"> Author id :' . $row['Employee_id'] .'</div>'.
+'<div class="date">'."Date :". $row['date_ps'].'</div>'.'<br>'.
+'<div class="header">'. $row['Header'].'</div>'."<br>".
+'<div class="text">'. $row['Text_ps'].'</div>'."<br>".
+    '</div>';
+
+}
+?>
 <html>
  <style>
+.poster{
+font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; 
+padding: 20px 10px 20px 30px;
+background-color: whitesmoke ;
+border: 10px; 
+border-style: solid;
+border-color: rgb(245, 228, 228) ;  
+}
+
 .header{
-font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;    
+
+align-items: center;   
 }
 
  </style>  
+ 
 </html>
 <?php
-}
-for($page=1;$page<=$pagenumber;$page++)
+for($page=1;$page<= $pagenumber;$page++)
 {
-echo ('<a href = newspage.php? page="'.$page.'>'.$page.'</a>');    
+echo '<a href = "newspage.php?page='.$page.'">'.$page.'</a>';    
 }
 
 ?>
